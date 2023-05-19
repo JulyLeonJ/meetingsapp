60a10f4fa91d801f21702eec9300997b94b6da96
jjleon22 <leonjairetsell@gmail.com>
2023-05-18 19:28:44 -0500

    add imports index and add config
    - Importan librerias en index
    - Añadir configuraciones en conexion a la bd

---
+++ controller/reuniones.controller.py
@@ -0,0 +1,2 @@
+from flask import render_template, redirect, request, session, session, flash
+from model import reuniones
--- index.py
+++ index.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template 
+from vidstream import *
+import tkinter as tk
+import socket
+import threading
+import requests
                                                     #importando flask
 app = Flask (__name__,template_folder='templates')
                                                     #creando app 
@@ -12,3 +17,4 @@ if __name__ == '__main__':
     
     app.run(debug=True)
                                                     #corriendo app 
+local_ip_address = socket.gethostbyname(socket.gethostname())
